0370475bee881da58d2a28fbacb0eb0720877a4f
coniekp <coniekp@icloud.com>
2018-07-10 16:04:55 -0700

    Set up client side and server side bare-min

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,23 @@
+$(document).ready(()=>{
+  
+  var $submit = $('form').on( 'submit', (e)=> {
+    e.preventDefault();
+    var val = $('textarea').val(); 
+    post(val, renderRows);
+  });
 
-
+  post = (input, cb) => {
+    jQuery.post('http://127.0.0.1:3000/hi', {data: input}, (data) => {
+      cb(data);
+    });
+  }
+  
+  renderRows = (rows) => {
+    rows.forEach(row => {
+      var text = row.join(',');
+      var $record = $('<div>').text(text);
+      $('.results').append($record);
+    });
+  };
+  
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,57 @@
+var express = require('express');
+var server = express();
+var bodyParser = require('body-parser');
 
+
+convertToCSV = (req, res, next) => {
+  var input = JSON.parse(req.body.data);
+  var fields = getFieldsInData(input);
+  var output = flattenDataStructure(fields, input);
+  
+  res.send(output);
+}
+
+getFieldsInData = (data) => {
+  var fields = [];
+  for (var key in data) {
+    if (key !== 'children') {
+        fields.push(key);
+    }
+  }
+  return fields;
+};
+
+flattenOneRecord = (fields, data) => {
+  return fields.map(field => {
+    return data[field];
+  });
+};
+
+flattenDataStructure = (fields, data) => {
+  var results = [fields];
+  
+  var recurse = (obj) => {
+    results.push(flattenOneRecord(fields, obj));
+    if(obj.children) {
+      obj.children.forEach(child => recurse(child));
+    }
+  }
+  
+  recurse(data);
+  return results;  
+}
+
+
+server.use(express.static('client'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(convertToCSV);
+
+
+server.post('/hi', (req, res) => {
+  console.log("after send", JSON.parse(req.body.data));
+});
+
+server.listen(3000, ()=>{
+  console.log('LESTENENG')});
 
